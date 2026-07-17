6496f0168a1b6127799960fde06195e95d0a9386
knownothing20 <137740359+knownothing20@users.noreply.github.com>
2026-07-18 00:00:49 +0800

    chore(client): export development session client

--- packages/client-core/index.js
+++ packages/client-core/index.js
@@ -1,5 +1,6 @@
 export * from "./client-runtime.js";
 export * from "./client-state.js";
 export * from "./connection-registry.js";
+export * from "./development-sessions.js";
 export * from "./endpoint-selector.js";
 export * from "./project-profile.js";
